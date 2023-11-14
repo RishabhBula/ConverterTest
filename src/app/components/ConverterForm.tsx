5d91a769d526be30ad8dc7e67c8444b54572f56a
Rishabh Bula <rishabhbula1712@gmail.com>
2023-11-14 19:23:05 +0530

    modified the code

--- src/app/components/ConverterForm.tsx
+++ src/app/components/ConverterForm.tsx
@@ -10,6 +10,9 @@ import { getApi } from '../api'
 const ConverterForm = () => {
   const [listData, setListData] = useState([])
   const [showDropdown , setShowDropdown] = useState<boolean>(false)
+  const [selectedData ,setSelectedData] = useState<string>('bnb')
+  const [defaultSelect ,setDefaultSelect] = useState<string>('')
+
   useEffect(() => {
     getApi('/coins/list')
       .then((res: any) => {
@@ -29,27 +32,31 @@ const ConverterForm = () => {
 
          <div className="pt-20 w-[402px] mx-auto">
             <div className="w-full relative mb-7">
-            <input type="number" className="border-b-2 border-[#e4e4e414] pl-4 h-[63px] bg-[rgba(228,228,228,0.08)] w-full"/>
+            <input type="number" className="border-b-2 border-[#e4e4e414] pl-4 min-h-[63px] h-max p-2 bg-[rgba(228,228,228,0.08)] w-full"/>
             <span className="flex flex-cols gap-5 justify-center items-center h-full absolute top-0 right-[14px] text-[#E4E4E4]">
-                <Image className="object-center object-contain" src={BNB} alt="BNB"/>
+                <span className="uppercase text-bold text-[20px]">{selectedData}</span>
+                {/* <Image className="object-center object-contain" src={BNB} alt="BNB"/> */}
                 <Image className="object-center object-contain" src={logoCrypto} alt="logoCrypto"/>
             </span>
             </div>
            
            <div className="w-full relative mb-7" onClick={() => setShowDropdown(!showDropdown)}>
-              <input type="number" className="border-b-2 border-[#e4e4e414] pl-4 h-[63px] bg-[rgba(228,228,228,0.08)] w-full"/>
-              
+              {/* <input type="number" /> */}
+              <div className="flex justify-start text-[20px] items-center border-b-2 border-[#e4e4e414] pl-4 h-[63px] bg-[rgba(228,228,228,0.08)] w-full">
+                {defaultSelect}
               <span className="cursor-pointer font-bold flex justify-center items-center h-full absolute top-0 right-[14px] text-[#E4E4E4]">
                 Select
                 <Image className="object-center object-contain" src={arrow} alt="Arrow"/>
             </span>
+              </div>
             {
               showDropdown &&
             <ul className={`max-h-[200px] overflow-y-auto absolute left-0 top-[64px] bg-black/[80%] w-full`}>
               {/* {console.log(listData ,'listData')} */}
-            {listData?.map((ele:any, index:number)=>{
+            {listData?.slice(0,30).map((ele:any, index:number)=>{
               return(
-                <li key={index} className="overflow-hidden px-4 cursor-pointer py-2 hover:bg-white/[30%]">{ele.name}</li>
+                <li key={index} className="overflow-hidden px-4 cursor-pointer py-2 hover:bg-white/[30%]"
+                onClick={() => {setSelectedData(ele.symbol) , setDefaultSelect(ele.name)}}>{ele.name}</li>
               )
             })}
             </ul>
